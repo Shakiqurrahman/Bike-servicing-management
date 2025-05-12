7a1dd8fadbeab6e5f5e16911f614ee46c4d43e20
Shakiqurrahman <rahmanshakiqur@gmail.com>
2025-05-13 01:01:44 +0600

    vercel updated

--- dist/utils/prisma.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_client_1 = require("../../prisma/generated/prisma-client");
-const prisma = new prisma_client_1.PrismaClient();
-exports.default = prisma;
